125d5e3ebd494b1ecdd913e2294d2924dff41d7c
Dawood <83209168+14685-562@users.noreply.github.com>
2021-07-05 19:42:48 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -340,14 +340,20 @@ async def reportabug(ctx):
                     result = True
             except discord.ext.commands.errors.CommandInvokeError(e):
                 print(discord.ext.commands.errors.CommandInvokeError(e).original)
-                
+                print(e)                
                 if ((AImage[:7] != "https://") and (AImage[-4:] != ".png" and AImage[-4:] != ".jpg" and AImage[-5:] != ".jpeg" and AImage[-4:] != ".gif")) and AImage.lower() != "none" and AImage.lower() != "cancel":
                     await ctx.send('Please send a valid link/image, not just some text!')
                     MessageReply = await client.wait_for('message', check=lambda message: message.author.id == ctx.author.id)
                     AImage = (MessageReply.content)
+                else:
+                    result = True
              except HTTPException:
-                
-                    
+                if ((AImage[:7] != "https://") and (AImage[-4:] != ".png" and AImage[-4:] != ".jpg" and AImage[-5:] != ".jpeg" and AImage[-4:] != ".gif")) and AImage.lower() != "none" and AImage.lower() != "cancel":
+                    await ctx.send('Please send a valid link/image, not just some text!')
+                    MessageReply = await client.wait_for('message', check=lambda message: message.author.id == ctx.author.id)
+                    AImage = (MessageReply.content)
+                else:
+                    result = True
                 
             else:
                 break
