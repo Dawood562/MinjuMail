f623821216b74077f45216b2bb9ae75ff6d13a1c
Dawood <83209168+14685-562@users.noreply.github.com>
2021-08-25 22:46:22 +0100

    Set up cog
    Added a ping command

--- cogs/testcmds.py
+++ cogs/testcmds.py
@@ -1,1 +1,13 @@
 import discord
+from discord.ext import commands
+
+class testcmds(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.send(f'**Latency:** {round(client.latency*1000, 1)}ms')
+
+def setup(client):
+    client.add_cog(testcmds(client))
