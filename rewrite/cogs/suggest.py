609efe94c132faf3d6af1a5a6e811f56ac380e79
Dawood <83209168+14685-562@users.noreply.github.com>
2021-08-25 23:47:36 +0100

    Update suggest.py

--- rewrite/cogs/suggest.py
+++ rewrite/cogs/suggest.py
@@ -1,1 +1,85 @@
 import discord
+from datetime import datetime
+from discord.ext import commands
+
+class suggest(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command(aliases=['sac', 'scard', 'scards', 'cardsuggest', 'cs'])
+    @discord.ext.commands.dm_only()
+    @commands.cooldown(1,900,commands.BucketType.user)
+    async def suggestacard(self, ctx):
+        print(f'{datetime.now().strftime("%d/%m/%Y %H:%M:%S")}::: Card Suggestion started by {ctx.message.author}')
+        while True:
+            await ctx.send(embed=discord.Embed(color=random.choice(embedcolours), title='Thanks for wanting to suggest an artist!', description='Please remember that this should only be used if the artist is not already in the game or in the <#737721977816743966> channel!\nIf at any time you wish to cancel (e.g. if the bog bugs and 2 embeds are sent), type `cancel`! It takes until the 3rd embed for it to cancel, however.'))
+            QGroup = discord.Embed(color=random.choice(embedcolours), title="Artist Name (Without special characters):")
+            await ctx.send(embed=QGroup)
+            MessageReply = await self.client.wait_for('message', check=lambda message: message.author.id == ctx.author.id)
+            AArtist = (MessageReply.content)
+            # Check to cancel
+            if AArtist.lower() == "cancel":
+                break
+            else:
+                pass
+            ##################
+            QGender = discord.Embed(color=random.choice(embedcolours), title='Artist Gender (Male, Female, or Mixed):')
+            await ctx.send(embed=QGender)
+            MessageReply = await self.client.wait_for('message', check=lambda message: message.author.id == ctx.author.id)
+            AGender = (MessageReply.content)
+            # Check to cancel
+            if AGender.lower() == "cancel":
+                break
+            else:
+                pass
+            ##################
+            QType = discord.Embed(color=random.choice(embedcolours), title="Artist Type (Group/Soloist):")
+            await ctx.send(embed=QType)
+            MessageReply = await self.client.wait_for('message', check=lambda message: message.author.id == ctx.author.id)
+            AType = (MessageReply.content)
+            # Check to cancel
+            if AType.lower() == "cancel":
+                break
+            else:
+                pass
+            ##################
+            ECheck = discord.Embed(title="Card Suggestion", description="Please check the contents below to make sure everything is typed properly!", color=random.choice(embedcolours))
+            ECheck.add_field(name="**Artist Name:**", value=f"{AArtist}", inline=False)
+            ECheck.add_field(name="**Artist Gender:**", value=f"{AGender}", inline=False)
+            ECheck.add_field(name="**Artist Type:**", value=f"{AType}", inline=False)
+            ECheck.set_footer(text=f'{ctx.author} ({ctx.author.id})')
+            DMEmbed = await ctx.send(embed=ECheck)
+            await DMEmbed.add_reaction('❌')
+            await DMEmbed.add_reaction('✅')
+            print(f'{datetime.now().strftime("%d/%m/%Y %H:%M:%S")}::: Adds both reactions')
+            try:
+                reaction, user = await self.client.wait_for('reaction_add', check=lambda reaction, user: user == ctx.author,  timeout = 60.0)
+            except asyncio.TimeoutError:
+                print(f'{datetime.now().strftime("%d/%m/%Y %H:%M:%S")}::: A card suggestion by {ctx.message.author} timed out.')
+                return await ctx.send('Validation timed out. Please try again.')
+            else:
+                if reaction.emoji == '✅':
+                    await ctx.send("Thanks for the suggestion! It's been sent to the Minju Managers to verify the information!")
+                    await DMEmbed.remove_reaction('❌', self.client.user)
+                    await DMEmbed.remove_reaction('✅', self.client.user)
+
+                    ECheck.set_footer(text=f'Suggested by {ctx.author} ({ctx.author.id})')
+                    ECheck.set_author(name=f'{ctx.message.channel.id}/{DMEmbed.id}')
+                    CHECKEmbed = await self.client.get_channel(861687628881199104).send(f"New card suggestion from **{ctx.message.author}** ({ctx.message.author.mention}).", embed=ECheck)
+                    await CHECKEmbed.edit(content=f"New card suggestion from **{ctx.message.author}** ({ctx.message.author.mention}).\nUse `_accept {CHECKEmbed.id}` or `_reject {CHECKEmbed.id}` to accept or reject this card suggestion.\nAlso remember that they can't submit special characters so don't reject for that!", embed=ECheck)
+                    print(f'{datetime.now().strftime("%d/%m/%Y %H:%M:%S")}::: Card suggestion by {ctx.message.author} has been sent to the checking channel.')
+                    break
+                elif reaction.emoji == '❌':
+                    # Remove all reactions
+                    CLDEmbed = discord.Embed(title='Card suggestion has been cancelled.', description='Please reuse the command to redo your entries!', color=random.choice(embedcolours))
+                    await DMEmbed.edit(embed=CLDEmbed)
+                    await DMEmbed.remove_reaction('❌', self.client.user)
+                    await DMEmbed.remove_reaction('✅', self.client.user)
+                    print(f'{datetime.now().strftime("%d/%m/%Y %H:%M:%S")}::: Card suggestion by {ctx.message.author} has been cancelled.')
+                    break
+
+                else:
+                    return await ctx.send(f'You reacted with {reaction}... start again.')
+                    break
+def setup(client):
+    client.add_cog(suggest(client))
