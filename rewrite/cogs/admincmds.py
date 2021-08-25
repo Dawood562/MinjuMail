7059f80ce1dccc34fa346eaf3bac44abdc616634
Dawood <83209168+14685-562@users.noreply.github.com>
2021-08-25 23:20:35 +0100

    Update admincmds.py

--- rewrite/cogs/admincmds.py
+++ rewrite/cogs/admincmds.py
@@ -7,7 +7,7 @@ class admincmds(commands.Cog):
         self.client = client
 
 
-    @client.command()
+    @commands.command()
     async def restart(ctx):
         if ctx.author.id != 221188745414574080:
             pass
@@ -15,7 +15,7 @@ class admincmds(commands.Cog):
             await ctx.send("Restarting bot...")
             os.execv(sys.executable, ['python'] + sys.argv)
 
-    @client.command(aliases=['goodnight', 'jaljjayo', 'sd', 'snowwhendubu', 'maliwhensunoo'])
+    @commands.command(aliases=['goodnight', 'jaljjayo', 'sd', 'snowwhendubu', 'maliwhensunoo'])
     async def shutdown(ctx):
         if (ctx.message.author.id != 221188745414574080) and (ctx.message.author.id != 303901339891531779):
             print(f'{datetime.now().strftime("%d/%m/%Y %H:%M:%S")}::: {ctx.message.author} tried to shut the bot down lol')
